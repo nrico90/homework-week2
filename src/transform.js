12d181ea1a2cbf2e3eebe2f124ebbdb3b9f64da5
nrico90 <nrico90@gmail.com>
2019-11-16 21:35:00 +0100

    last commit, finish

--- src/transform.js
+++ src/transform.js
@@ -1,36 +1,3 @@
-let bigGroup = {
-    "20 and younger": [],
-    "21 - 30": [],
-    "31 - 40": [],
-    "41 - 50": [],
-    "51 and older": []
-}
-
-function sortGroups(person) {
-    if (person.age <= 20) {
-        bigGroup["20 and yonger"].push(person)
-    }
-    else if ((person.age < 31) && (person.age > 20)) {
-        bigGroup["21 - 30"].push(person)
-    }
-    else if ((person.age < 41) && (person.age > 30)) {
-        bigGroup["31 - 40"].push(person)
-    }
-    else if ((person.age < 51) && (person.age > 40)) {
-        bigGroup["41 - 50"].push(person)
-    }
-    else if (person.age > 50) {
-        bigGroup["51 and older"].push(person)
-    }
-    // if (bigGroup["20 and yonger"] === 0) delete bigGroup["20 and yonger"]
-    // if (bigGroup["21 - 30"] === 0) delete bigGroup["21 - 30"]
-    // if (bigGroup["31 - 40"] === 0) delete bigGroup["31 - 40"]
-    // if (bigGroup["41 - 50"] === 0) delete bigGroup["41 - 50"]
-    // if (bigGroup["51 and older"] === 0) delete bigGroup["51 and older"]
-
-    return bigGroup
-  }
-
 function groupAdultsByAgeRange(people) {
     if (people.length === 0) {
       return {}  
@@ -41,10 +8,33 @@ function groupAdultsByAgeRange(people) {
                     if (person.age >= 18) return person
                     else return false
               })
-            .reduce(sortGroups, {})
+            // i had a lof of struggles with checking if the array has the existing key "!", i was stuck for hour on unidentified "age"
+            // also it took me a while to see that there was spaces in "21-30" etc. it kept giving me error with the "push"
+            .reduce(function (bigGroup, person) {
+                if (person.age <= 20) {
+                    if (!bigGroup["20 and younger"]) bigGroup["20 and younger"] = []
+                    bigGroup["20 and younger"].push(person)
+                }
+                else if ((person.age > 20) && (person.age < 31)) {
+                    if (!bigGroup["21-30"]) bigGroup["21-30"] = []
+                    bigGroup["21-30"].push(person)
+                }
+                else if ((person.age > 30) && (person.age < 41)) {
+                    if (!bigGroup["31-40"]) bigGroup["31-40"] = []
+                    bigGroup["31-40"].push(person)
+                }
+                else if ((person.age > 40) && (person.age < 51)) {
+                    if (!bigGroup["41-50"]) bigGroup["41-50"] = []
+                    bigGroup["41-50"].push(person)
+                }
+                else if (person.age > 50) {
+                    if (!bigGroup["51 and older"]) bigGroup["51 and older"] = []
+                    bigGroup["51 and older"].push(person)
+                }
+                return bigGroup
+              }
+            , {})
     }
 }
 
-
-//const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
-module.exports = {groupAdultsByAgeRange}
+module.exports = { groupAdultsByAgeRange }
